34062dcdea94f72012a2a27c45b20a01b84941dc
RahulGupta-8319 <guptagi5050@gmail.com>
2022-12-16 15:53:20 +0530

    client

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import { Link } from 'react-router-dom'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    < div className='main-container'>
+    <div className="first">
+     <h1>Enter a long url</h1>
+     <input type='text' /><br/><br/>
+     <button>Make it sort</button>
+    </div>
+    <div className='second'>
+      <h1>Welcome gUy's</h1>
+      <p>
+      I create a web like Tinyurl for easily share a long url. and implemnent cashing for quick response .
+      Used Redis caching for low network latency and to improve user experience
+      </p>
+      <a href='https://github.com/RahulGupta-8319/backend-development' target='_blank'>Github Link</a>     {/* //link not working here why...?? */}
+    
+    </div>
     </div>
   );
 }
